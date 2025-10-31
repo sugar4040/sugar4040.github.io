283de00132d65c5bcbe76866c283a28c907441d8
[sugar4040] <[shun.sugar.2@gmail.com]>
2025-10-31 10:29:41 +0900

    v

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ class ATMSystem {
 
         // マカイドルの場合はフォントサイズを半分に
         if (this.currentCurrency === 'makai') {
-            currencyElement.style.fontSize = '79px';
+            currencyElement.style.fontSize = '811px';
         } else {
             currencyElement.style.fontSize = '100px';
         }
